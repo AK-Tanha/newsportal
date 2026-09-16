41788f917994612c551caeb12d2b0653f5ab0a00
AK Tanha <aktanha75@gmail.com>
2026-09-16 17:58:05 +0600

    fixed

--- lib/locales.ts
+++
@@ -1,9 +0,0 @@
-export const locales = ["bn", "en"] as const;
-
-export type Locale = (typeof locales)[number];
-
-export const defaultLocale: Locale = "bn";
-
-export function isLocale(value: string): value is Locale {
-  return (locales as readonly string[]).includes(value);
-}
